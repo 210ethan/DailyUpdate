273f1bfa342084d1b8db6a01ce78ffb9272149a3
ethanmorse <ethan.morse97@gmail.com>
2017-04-05 13:03:50 -0500

    Add files via upload

---
+++ DailyUpdateInfoExample.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 13:01:48 2017
+
+@author: EthanMorse
+"""
+
+# get_weather
+owmKey = "OpenWeatherMapAPIKey"
+owmIDs = {"City" : CityID , "City" : CityID}
+
+# get_stock_info
+tickerList = ["ticker"]
+
+# get_news
+newsKey = "NewsAPIkey"
+
+# get_todo_list
+username = "Reddit ssername"
+password = "Reddit password"
+client_id = "App client_id"
+client_secret = "App client_secret"
